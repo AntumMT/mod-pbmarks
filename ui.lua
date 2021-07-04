7cc76a117ad09ffb5567e39451a20d895571dc53
Jordan Irwin <antumdeluge@gmail.com>
2021-07-04 16:53:49 -0700

    Add i3 support (WIP)

--- ui.lua
+++ ui.lua
@@ -2,12 +2,29 @@
 local S = core.get_translator(pbmarks.modname)
 
 local ui_handlers = {
+	"i3",
 	"sfinv_buttons",
 	"unified_inventory",
 }
 
 local handler_available = false
 
+
+if core.global_exists("i3") then
+	i3.new_tab({
+		name = pbmarks.modname,
+		description = S("Personal Bookmarks"),
+		formspec = function(player, data, fs)
+			-- FIXME:
+			--   - use i3 widgets
+			--   - inventory widgets are missing after close
+			i3.set_tab(player, "inventory")
+			pbmarks.show_formspec(player:get_player_name(), {noback=true})
+		end,
+	})
+
+	handler_available = true
+end
 
 if core.global_exists("sfinv_buttons") then
 	sfinv_buttons.register_button(pbmarks.modname, {
